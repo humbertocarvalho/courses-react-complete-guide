bb91ea12158bed949dfd3a6a4194d7fce9af0e8b
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-07-15 18:40:23 -0300

    Crated Person.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className='App'>
       <h1>Hi, Im a react app</h1>
+      <p>Will this work?</p>
+      <Person />
     </div>
   );
 }
