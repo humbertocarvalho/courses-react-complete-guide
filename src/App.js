647f3f1d365d2c0d35e6529fdff6d182dd2ed2f5
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-07-16 21:54:54 -0300

    Outputting lists.

--- src/App.js
+++ src/App.js
@@ -67,18 +67,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            click={this.switchNameHandler.bind(this, 'Humberto!!')}
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            changed={this.nameChangedHandler}
-          >
-            My Hobbies: Racing
-          </Person>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
