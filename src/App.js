a550f4097fdac1547dcf3d05a4b0bed06bb389e6
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-07-17 17:57:39 -0300

    Add.

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ class App extends Component {
     });
 
     const person = { ...this.state.persons[personIndex] };
-
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
+
     this.setState({
       persons
     });
